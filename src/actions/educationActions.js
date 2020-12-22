b3c2297e423f69c65325b9d2799e4877df074960
Archit garg <architgarg603@gmail.com>
2020-12-22 23:05:42 +0530

    added actions

--- src/actions/educationActions.js
+++ src/actions/educationActions.js
@@ -1,12 +1,21 @@
 export const updateEducation = (educationDetails)=>{
 
-    return (dispatch , getState)=>{
-        
-        // async calls
-
-
-
-        dispatch({  type:"UPDATE_EDUCATION" , educationDetails:educationDetails })
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
+        // aasync taskss
+    
+        // firebase => update doc
+        let db = getFirestore();
+        // console.log(getState());
+        let uid = getState().firebase.auth.uid;
+    
+        db.collection("resumes").doc(uid).set(
+          {
+            educationDetails: educationDetails,
+          },
+          { merge: true }
+        ).then(()=>{
+            dispatch({  type:"UPDATE_EDUCATION" , educationDetails:educationDetails })
+        })
     }
     
     
